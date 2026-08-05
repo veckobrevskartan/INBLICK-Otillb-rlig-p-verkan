8fc68c1237db1b1154cdc067d6998be45b86c153
veckobrevskartan <veckobrevskartan@proton.me>
2026-08-05 13:09:42 +0200

    Blockera pensionerade och generiska källadresser

--- scripts/validate-references.mjs
+++ scripts/validate-references.mjs
@@ -29,7 +29,52 @@ const forbiddenUrls = new Set([
   'https://www.svt.se/nyheter/lokalt/skane/',
   'https://www.aklagare.se/nyheter-press/pressmeddelanden/',
   'https://www.publikt.se/amne/tingsratten',
-  'https://www.domstol.se/goteborgs-tingsratt/om-tingsratten/kontakta-oss/'
+  'https://www.domstol.se/goteborgs-tingsratt/om-tingsratten/kontakta-oss/',
+  "https://bra.se/rapporter/arkiv/2024-03-01-mojliggorare-for-kriminella-natverk",
+  "https://edition.cnn.com/2024/07/26/sport/paris-olympics-opening-ceremony-train-sabotage/index.html",
+  "https://foreignpolicy.com/2024/05/13/russia-gru-sabotage-europe-arson-criminal-network/",
+  "https://foreignpolicy.com/2024/06/13/russia-sabotage-attacks-europe-espionage-hybrid-arson/",
+  "https://lakartidningen.se/aktuellt/nyheter/2020/01/medhelp-ska-betala-12-miljoner-for-1177-lackan/",
+  "https://sverigesradio.se/artikel/kriminalvardarna-som-byter-sida",
+  "https://www.bellingcat.com/news/2021/04/20/senior-gru-leader-directly-involved-with-czech-arms-depot-explosion/",
+  "https://www.bellingcat.com/news/uk-and-europe/2021/04/19/gru-unit-29155-and-the-czech-ammunition-depots/",
+  "https://www.bloomberg.com/news/articles/2024-07-26/paris-olympics-rail-sabotage-sncf",
+  "https://www.dagenssamhalle.se/ekonomi/kommunekonomi/chef-sparkad-misstanks-ha-stulit-miljoner-fran-skaanska-kommuner/",
+  "https://www.dn.se/nyheter/sverige/atalas-for-att-ha-beviljat-66-arbetstillstand/",
+  "https://www.dn.se/nyheter/sverige/utbetalningsmyndigheten-nekades-placering-i-sodertalje/",
+  "https://www.etc.se/inrikes/gaengkopplad-naemndeman-tvingas-fraan-moderaterna-skulle-driva-hvb-hem",
+  "https://www.expressen.se/nyheter/brottscentralen/advokat-med-232-timmars-kontakt-med-livstidsdomd-utesluts/",
+  "https://www.expressen.se/nyheter/brottscentralen/klanadvokaten-uteslots-ur-advokatsamfundet-nu-ar-han-tillbaka/",
+  "https://www.expressen.se/nyheter/brottscentralen/kriminalvardaren-gifte-sig-med-bandidos-man-bombhotet-foljde/",
+  "https://www.expressen.se/nyheter/brottscentralen/kriminalvardaren-hotades-till-narkotikabrott/",
+  "https://www.expressen.se/nyheter/krim/kvinna-tog-jobb-pa-haktet-for-att-hjalpa-gangledarens-fange/",
+  "https://www.gp.se/ekonomi/bankman-godkande-bolan-med-falska-anstallningsbevis",
+  "https://www.gp.se/nyheter/g%C3%B6teborg/radikala-islamisters-bolag-tj%C3%A4nar-miljoner-p%C3%A5-myndigheter-1.33313573",
+  "https://www.gp.se/nyheter/goteborg/bankman-domdes-till-fangelse-for-att-ha-lurat-seb.6df8e5a4-90ce-4ae0-a59a-cb27a7cb87f9",
+  "https://www.justice.gov/usao-mdla/pr/westlake-man-sentenced-computer-fraud-charges-after-hacking-former-employer",
+  "https://www.msb.se/sv/amnesomraden/informationssakerhet-cybersakerhet-och-sakra-kommunikationer/",
+  "https://www.ncsc.gov.uk/alerts/operation-cloud-hopper",
+  "https://www.nyteknik.se/sakerhet/ericssonspionen-fick-tre-ar-6386124",
+  "https://www.oxfordmail.co.uk/news/23560266.ashley-liles-jailed-following-cyber-attack-oxford-biomedica/",
+  "https://www.publikt.se/nyhet/skatteanstallda-doms-for-grovt-bedrageri-19892",
+  "https://www.riksrevisionen.se/granskningar/granskningsrapporter/2018/skyddet-mot-oegentligheter-i-migrationsverksamheten.html",
+  "https://www.riksrevisionen.se/granskningar/granskningsrapporter/2026/infiltration-i-staten---tre-myndigheters-arbete-for-att-motverka-illojalt-beteende.html",
+  "https://www.riksrevisionen.se/rapporter/granskningsrapporter/2023/informationssakerhet-vid-universitet-och-hogskolor---hanteringen-av-skyddsvarda-forskningsdata.html",
+  "https://www.sakerhetspolisen.se/hoten-mot-sverige/underrattelsehotet.html",
+  "https://www.sakerhetspolisen.se/kontraspionage/industrispionage.html",
+  "https://www.sakerhetspolisen.se/kontraspionage/ryssland.html",
+  "https://www.sakerhetspolisen.se/om-sakerhetspolisen/publikationer/sakerhetspolisens-arsberattelse/sakerhetspolisens-2023-2024/allvarligt-lage/ryssland-kina-och-iran.html",
+  "https://www.svd.se/a/unga-kvinnor-infiltrerar-hakten",
+  "https://www.svt.se/nyheter/inrikes/handlaggare-beviljade-66-tillstand-sista-dagen-pa-jobbet",
+  "https://www.svt.se/nyheter/lokalt/skane/hamnanstallda-smugglade-118-kilo-narkotika",
+  "https://www.svt.se/nyheter/lokalt/sodermanland/region-sormland-far-betala-250-000-kr-for-1177-lackan",
+  "https://www.svt.se/nyheter/lokalt/vast/anstalld-pa-kriminalvarden-smugglade-meddelanden",
+  "https://www.svt.se/nyheter/lokalt/vast/socialsekreterare-fejkade-examensbevis",
+  "https://www.theregister.com/2015/02/18/it_admin_jailed_for_sabotaging_employer_systems/",
+  "https://www.theregister.com/2023/05/31/it_analyst_guilty_blackmail/",
+  "https://www.trendmicro.com/en_us/research/17/a/operation-cloud-hopper.html",
+  "https://www.trendmicro.com/vinfo/pl/security/news/cyber-attacks/operation-cloud-hopper-what-you-need-to-know",
+  "https://www.unitedagainstnucleariran.com/report/irans-irgc-at-swedish-universities",
 ]);
 
 for (const item of cases) {
@@ -46,13 +91,14 @@ for (const item of cases) {
     if (parsed.protocol !== 'https:') errors.push(`${item.id}: källan använder inte HTTPS: ${source.url}`);
     if (forbiddenHosts.has(parsed.hostname)) errors.push(`${item.id}: social medie-länk får inte vara kanonisk källa: ${source.url}`);
     if ((parsed.pathname === '/' || parsed.pathname === '') && !parsed.search) errors.push(`${item.id}: länk går till startsida: ${source.url}`);
-    if (forbiddenUrls.has(source.url)) errors.push(`${item.id}: länk går till landnings-/översiktssida: ${source.url}`);
+    if (forbiddenUrls.has(source.url)) errors.push(`${item.id}: länk är känd som inaktuell eller går till landnings-/översiktssida: ${source.url}`);
+    if (/\/(search|sok|sök)(\/|$)/i.test(parsed.pathname) || parsed.searchParams.has('query')) errors.push(`${item.id}: sökresultat får inte användas som källa: ${source.url}`);
     urls.add(source.url);
   }
 }
 
 if (!modules.includes(`const CASES=${JSON.stringify(cases)};`)) errors.push('Falldatan i modules.html avviker från cases.json');
-if (cases.length !== 115) errors.push(`Förväntade 115 fall, hittade ${cases.length}`);
+if (cases.length !== 107) errors.push(`Förväntade 107 fall, hittade ${cases.length}`);
 
 if (errors.length) {
   console.error(errors.map(error => `- ${error}`).join('\n'));
